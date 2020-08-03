3cf103ebe9567995d4ee0da8e00fc332d5272de8
Chilcano <chilcano@intix.info>
2020-08-03 19:45:44 +0200

    Update to TF 0.12

--- main.tf
+++ main.tf
@@ -6,51 +6,51 @@ resource "aws_vpc" "main_vpc" {
   enable_dns_hostnames = true
 
   tags {
-    Name = "${var.node_name}"
-    Environment = "${var.node_name}"
+    Name = var.node_name
+    Environment = var.node_name
   }
 }
 
 resource "aws_internet_gateway" "main_gw" {
-  vpc_id = "${aws_vpc.main_vpc.id}"
+  vpc_id = aws_vpc.main_vpc.id
 
   tags {
-    Name = "${var.node_name}"
-    Environment = "${var.node_name}"
+    Name = var.node_name
+    Environment = var.node_name
   }
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id = "${aws_vpc.main_vpc.id}"
+  vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.100.0/24"
   availability_zone = "${var.region}${var.az}"
   map_public_ip_on_launch = true
 
   tags {
-    Name = "${var.node_name}"
-    Environment = "${var.node_name}"
+    Name = var.node_name
+    Environment = var.node_name
   }
 }
 
 resource "aws_route_table" "gw_route_table" {
-  vpc_id = "${aws_vpc.main_vpc.id}"
+  vpc_id = aws_vpc.main_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.main_gw.id}"
+    gateway_id = aws_internet_gateway.main_gw.id
   }
 
-  depends_on = ["aws_internet_gateway.main_gw"]
+  depends_on = [aws_internet_gateway.main_gw]
 
   tags {
-    Name = "${var.node_name}"
-    Environment = "${var.node_name}"
+    Name = var.node_name
+    Environment = var.node_name
   }
 }
 
 resource "aws_route_table_association" "public_route_table" {
-  subnet_id = "${aws_subnet.public_subnet.id}"
-  route_table_id = "${aws_route_table.gw_route_table.id}"
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.gw_route_table.id
 }
 // ------------------------------------------------------
 
@@ -58,13 +58,13 @@ resource "aws_route_table_association" "public_route_table" {
 // Security Group and its ingress/egress rules   
 // ======================================================
 resource "aws_security_group" "sec_group" {
-  name = "${var.node_name}"
+  Name = var.node_name
   description = "Allow inbound and outbound traffic"
-  vpc_id = "${aws_vpc.main_vpc.id}"
+  vpc_id = aws_vpc.main_vpc.id
 
   tags {
-    Name = "${var.node_name}"
-    Environment = "${var.node_name}"
+    Name = var.node_name
+    Environment = var.node_name
   }
 }
 
@@ -73,8 +73,8 @@ resource "aws_security_group_rule" "allow_all_from_self" {
   from_port       = 0
   to_port         = 0
   protocol        = "-1"
-  source_security_group_id = "${aws_security_group.sec_group.id}"
-  security_group_id = "${aws_security_group.sec_group.id}"
+  source_security_group_id = aws_security_group.sec_group.id
+  security_group_id = aws_security_group.sec_group.id
 }
 
 resource "aws_security_group_rule" "allow_ssh_from_admin" {
@@ -82,8 +82,8 @@ resource "aws_security_group_rule" "allow_ssh_from_admin" {
   from_port       = 22
   to_port         = 22
   protocol        = "tcp"
-  cidr_blocks     = "${split(",", var.developer_cidr_blocks)}"
-  security_group_id = "${aws_security_group.sec_group.id}"
+  cidr_blocks     = split(",", var.developer_cidr_blocks)
+  security_group_id = aws_security_group.sec_group.id
 }
 
 resource "aws_security_group_rule" "allow_https_from_web" {
@@ -93,7 +93,7 @@ resource "aws_security_group_rule" "allow_https_from_web" {
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.sec_group.id}"
+  security_group_id = aws_security_group.sec_group.id
 }
 
 resource "aws_security_group_rule" "allow_http_from_web" {
@@ -103,7 +103,7 @@ resource "aws_security_group_rule" "allow_http_from_web" {
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.sec_group.id}"
+  security_group_id = aws_security_group.sec_group.id
 }
 
 resource "aws_security_group_rule" "allow_all_out" {
@@ -113,7 +113,7 @@ resource "aws_security_group_rule" "allow_all_out" {
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.sec_group.id}"
+  security_group_id = aws_security_group.sec_group.id
 }
 // ------------------------------------------------------
 
@@ -175,18 +175,18 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_ebs_policy_att" {
-  role       = "${aws_iam_role.ec2_iam_role.name}"
-  policy_arn = "${aws_iam_policy.ec2_ebs_policy.arn}"
+  role       = aws_iam_role.ec2_iam_role.name
+  policy_arn = aws_iam_policy.ec2_ebs_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_role_policy_att" {
-  role       = "${aws_iam_role.ec2_iam_role.name}"
+  role       = aws_iam_role.ec2_iam_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_iam_instance_profile" "iam_instance_profile" {
   name = "${var.node_name}_instance_profile"
-  role = "${aws_iam_role.ec2_iam_role.name}"
+  role = aws_iam_role.ec2_iam_role.name
 }
 // ------------------------------------------------------
 
@@ -202,7 +202,7 @@ data "template_file" "install_devops_tpl" {
   template = "${file("resources/cloudinit/install_devops_tpl.sh")}"
 
   vars {
-    instancename = "${var.node_name}"
+    instanceName = var.node_name
   }
 }
 
@@ -238,19 +238,19 @@ data "aws_ami" "latest_ami" {
 }
 
 resource "aws_spot_instance_request" "remotedesktop" {
-  ami                     = "${data.aws_ami.latest_ami.id}"
-  instance_type           = "${var.remotedesktop_instance_type}"
-  subnet_id               = "${aws_subnet.public_subnet.id}"
-  user_data_base64        = "${data.template_cloudinit_config.remotedesktop_userdata_cloudinit.rendered}"
-  key_name                = "${var.ssh_key}"
-  iam_instance_profile    = "${aws_iam_instance_profile.iam_instance_profile.name}"
-  vpc_security_group_ids  = ["${aws_security_group.sec_group.id}"]
-  spot_price              = "${var.remotedesktop_spot_price}"
+  ami                     = data.aws_ami.latest_ami.id
+  instance_type           = var.remotedesktop_instance_type
+  subnet_id               = aws_subnet.public_subnet.id
+  user_data_base64        = data.template_cloudinit_config.remotedesktop_userdata_cloudinit.rendered
+  key_name                = var.ssh_key
+  iam_instance_profile    = aws_iam_instance_profile.iam_instance_profile.name
+  vpc_security_group_ids  = [aws_security_group.sec_group.id]
+  spot_price              = var.remotedesktop_spot_price
   valid_until             = "9999-12-25T12:00:00Z"
   wait_for_fulfillment    = true
   private_ip              = "10.0.100.4"
 
-  depends_on = ["aws_internet_gateway.main_gw"]
+  depends_on = [aws_internet_gateway.main_gw]
 
   tags {
     Name = "${var.node_name}_spot_instance"
@@ -258,7 +258,7 @@ resource "aws_spot_instance_request" "remotedesktop" {
   }
 
   lifecycle {
-    ignore_changes = ["ami"]
+    ignore_changes = [ami]
   }
 }
 // ------------------------------------------------------
